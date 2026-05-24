2de3ae90503ac84d6ada7a032b37b9a96b892fa2
Damika-Anupama <damikaanupama@gmail.com>
2026-05-24 23:05:47 +0530

    style: redesign sign up demo flow

--- src/app/view/sign-up/sign-up.component.ts
+++ src/app/view/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  constructor(private router: Router) {}
 
+  submitDemoSignup(): void {
+    Swal.fire({
+      customClass: { popup: 'demo-detail-modal' },
+      icon: 'info',
+      title: 'Demo account flow',
+      text: 'This frontend demo uses sample data. Choose a demo role on the sign-in page to continue.',
+      confirmButtonText: 'Go to sign in'
+    }).then(() => this.router.navigate(['/sign-in']));
+  }
 }
